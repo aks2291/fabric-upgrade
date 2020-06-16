ccb591e0948555f89511ac35e148dd8ed664e9f0
Avinash Singh <aks2291@gmail.com>
2020-06-16 17:44:02 +0530

    change in fabric cmd

--- fabric_cmd.sh
+++ fabric_cmd.sh
@@ -25,12 +25,30 @@ peer lifecycle chaincode install ${CHAINCODE_NAME}.tar.gz
 peer lifecycle chaincode approveformyorg -C ${CHANNEL_NAME} -n ${CHAINCODE_NAME} -v ${VERSION} --sequence ${SEQUENCE} --init-required --package-id $PACKAGE_ID --tls true --cafile $ORDERER_CA --signature-policy "OR ('ORG1MSP.member','ORG2MSP.member')"
 peer lifecycle chaincode checkcommitreadiness -C ${CHANNEL_NAME} -n ${CHAINCODE_NAME} -v ${VERSION} --sequence ${SEQUENCE} --init-required --tls true --cafile $ORDERER_CA --signature-policy "OR ('ORG1MSP.member','ORG2MSP.member')" --output json
 
-peer lifecycle chaincode commit -o orderer.nipponexpress.com:7050 -C ${CHANNEL_NAME} -n ${CHAINCODE_NAME} -v ${VERSION} --sequence ${SEQUENCE} --init-required --tls true --cafile $ORDERER_CA --signature-policy "OR ('ORG1MSP.member','ORG2MSP.member')" \
+peer lifecycle chaincode commit -o orderer.brownbag.com:7050 -C ${CHANNEL_NAME} -n ${CHAINCODE_NAME} -v ${VERSION} --sequence ${SEQUENCE} --init-required --tls true --cafile $ORDERER_CA --signature-policy "OR ('ORG1MSP.member','ORG2MSP.member')" \
 --peerAddresses peer0.org1.brownbag.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.brownbag.com/peers/peer0.org1.brownbag.com/tls/ca.crt \
 --peerAddresses peer0.org2.brownbag.com:8051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.brownbag.com/peers/peer0.org2.brownbag.com/tls/ca.crt
 
 
-peer chaincode invoke -o orderer.nipponexpress.com:7050 --isInit --tls true --cafile $ORDERER_CA -C ${CHANNEL_NAME} -n ${CHAINCODE_NAME} \
+peer chaincode invoke -o orderer.brownbag.com:7050 --isInit --tls true --cafile $ORDERER_CA -C ${CHANNEL_NAME} -n ${CHAINCODE_NAME} \
 --peerAddresses peer0.org1.brownbag.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.brownbag.com/peers/peer0.org1.brownbag.com/tls/ca.crt \
 --peerAddresses peer0.org2.brownbag.com:8051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.brownbag.com/peers/peer0.org2.brownbag.com/tls/ca.crt \
 -c '{"Args":["createCar","CAR12","BMW","XL50","white", "Bob"]}' --waitForEvent
+
+
+# Org2 peer0 MSP
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.brownbag.com/users/Admin@org2.brownbag.com/msp
+CORE_PEER_LOCALMSPID=ORG2MSP
+CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.brownbag.com/peers/peer0.org2.brownbag.com/tls/server.crt
+CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.brownbag.com/peers/peer0.org2.brownbag.com/tls/server.key
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.brownbag.com/peers/peer0.org2.brownbag.com/tls/ca.crt
+CORE_PEER_ADDRESS=peer0.org2.brownbag.com:8051
+
+# Org1 peer0 MSP
+CH_NAME=mychannel
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.brownbag.com/users/Admin@org1.brownbag.com/msp
+CORE_PEER_LOCALMSPID=ORG1MSP
+CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.brownbag.com/peers/peer0.org1.brownbag.com/tls/server.crt
+CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.brownbag.com/peers/peer0.org1.brownbag.com/tls/server.key
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.brownbag.com/peers/peer0.org1.brownbag.com/tls/ca.crt
+CORE_PEER_ADDRESS=peer0.org1.brownbag.com:7051
